8c60c881580136a9ce1d06a2faf7e771a98a5822
Matthew Petty <mpetty-nayya@gmail.com>
2022-09-29 13:55:05 -0500

    ruby contract example

---
+++ lib/open_api/hr_open/employer_create_response.rb
@@ -0,0 +1,20 @@
+# example of generated dry contract
+# this was not generated
+
+module OpenApi
+    module HrOpen
+        class EmployerCreateResponse < Dry::Validation::Contract
+            params do
+                required(:employerOrganization).schema do
+                    required(:id).schema do
+                      required(:value).value(:string)
+                    end
+                    required(:name).filled(:string)
+                    optional(:id).schema do
+                        required(:value).value(:string)
+                    end
+                  end
+            end
+        end
+    end
+end
