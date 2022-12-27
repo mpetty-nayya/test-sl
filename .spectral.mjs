7f8b9626fc6b9f26a08f00884da23c8c41340e66
Lisa Carpenter <lisa@nayya.com>
2022-12-27 15:25:50 -0700

    Modified 17 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE1OTQ5NA/spectral.js?branch=hr-open-docs&token=c840fe63-20a8-4ecb-9072-ab2c0966647f";
+export default { extends: ruleset };
